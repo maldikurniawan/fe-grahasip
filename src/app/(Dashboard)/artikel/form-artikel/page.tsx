7deb57f5ff632f6f8bd8be90abe86050f25cf917
maldikurniawan <akurniawan2102@gmail.com>
2024-12-27 11:41:02 +0700

    add ckeditor

--- src/app/(Dashboard)/artikel/form-artikel/page.tsx
+++ src/app/(Dashboard)/artikel/form-artikel/page.tsx
@@ -20,21 +20,12 @@ const FormArtikel: React.FC = () => {
   const [isLoadingCkeditor, setLoadingCkeditor] = useState(true);
   const [refreshCkeditor, setRefreshCkeditor] = useState(false);
 
-  //Action API
+  // API actions
   const createArtikelApi = usePostData(API_URL_artikel, true);
-  const updateArtikelApi = usePutData(
-    API_URL_artikel + plainId + "/",
-    true
-  );
-  const getArtikelApi = useGetData(
-    `${API_URL_artikel + plainId + "/"}`,
-    [plainId],
-    true,
-    {},
-    { enabled: !!plainId }
-  );
+  const updateArtikelApi = usePutData(API_URL_artikel + plainId + "/", true);
+  const getArtikelApi = useGetData(`${API_URL_artikel + plainId + "/"}`, [plainId], true, {}, { enabled: !!plainId });
 
-  // Inisialisasi Formik
+  // Formik setup
   const formik = useFormik({
     initialValues: {
       title: "",
@@ -45,10 +36,8 @@ const FormArtikel: React.FC = () => {
     validationSchema: Yup.object({
       title: Yup.string().required("Title is required"),
       content: Yup.string().required("Content is required"),
-      image: Yup.string().required(""),
-      status: Yup.string()
-        .oneOf(["publish", "draft"], "Invalid status")
-        .required("Status is required"),
+      image: Yup.string().required("Image is required"),
+      status: Yup.string().oneOf(["publish", "draft"], "Invalid status").required("Status is required"),
     }),
     onSubmit: (values) => {
       const form = new FormData();
@@ -56,26 +45,27 @@ const FormArtikel: React.FC = () => {
       form.append("content", values.content);
       form.append("image", values.image);
       form.append("status", values.status);
-      //Update
+
       if (plainId) {
-        updateArtikelApi.mutate(form, {
+        // Update existing article
+        updateArtikelApi.mutate(form as any, {
           onSuccess: (res: any) => {
-            getArtikelApi.refetch();
-            router.push("/artikel");
             showToast(res.message, "success", 3000);
+            router.push("/artikel");
+            formik.resetForm(); // Reset form on success
           },
           onError: (error) => {
             console.log(error);
             errorResponse(error);
           },
         });
-      }
-      //create
-      else {
-        createArtikelApi.mutate(form, {
+      } else {
+        // Create new article
+        createArtikelApi.mutate(form as any, {
           onSuccess: (res: any) => {
             showToast(res.message, "success", 3000);
             router.push("/artikel");
+            formik.resetForm(); // Reset form on success
           },
           onError: (error) => {
             console.log(error);
@@ -86,7 +76,7 @@ const FormArtikel: React.FC = () => {
     },
   });
 
-  //config Ckeditor
+  // CKEditor config
   const toolbarConfig = {
     extraPlugins: "font,colorbutton,colordialog,justify,image2",
     removePlugins: "about,image",
@@ -94,68 +84,56 @@ const FormArtikel: React.FC = () => {
     height: "500",
   };
 
-  // Event handler ketika CKEditor siap
+  // Event handler when CKEditor is ready
   const onEditorReady = () => {
-    setLoadingCkeditor(false); // Sembunyikan loading indicator
+    setLoadingCkeditor(false); // Hide loading indicator
   };
 
-  //Collect data ketika Edit
+  // Collect data when editing
   useEffect(() => {
     if (param.get("id")) {
       const id = decrypted(param.get("id"));
       setPlainId(id);
     }
   }, [param]);
+
+  // Set form values when data is fetched
   useEffect(() => {
     if (getArtikelApi.isSuccess && getArtikelApi.data) {
       formik.setFieldValue("title", getArtikelApi.data?.title);
       formik.setFieldValue("content", getArtikelApi.data?.content);
-      formik.setFieldValue(
-        "image",
-        getArtikelApi.data?.image
-      );
+      formik.setFieldValue("image", getArtikelApi.data?.image);
       formik.setFieldValue("status", getArtikelApi.data?.status);
-      formik.setFieldValue("category", {
-        value: getArtikelApi.data?.category ?? "",
-        label: getArtikelApi.data?.category_name ?? "Select One",
-      });
     }
-    //eslint-disable-next-line
   }, [getArtikelApi.isSuccess]);
 
   useEffect(() => {
     if (formik.values.content && plainId) {
       setRefreshCkeditor(true);
     }
-    //eslint-disable-next-line
   }, [formik.values.content, plainId]);
 
   return (
     <Fragment>
       <div className="flex justify-between items-center">
         <h1 className="text-lg md:text-3xl font-bold transition-all">
-          <span>{param.get("id") ? "Edit" : "Tambah"}</span> Buku Panduan
+          <span>{param.get("id") ? "Edit" : "Tambah"}</span> Artikel
         </h1>
         <button
           className="text-xs whitespace-nowrap font-medium px-3 py-2 bg-gray-500 hover:bg-gray-600 active:bg-gray-600 text-white rounded-lg shadow hover:shadow-lg transition-all"
-          onClick={() => router.push("/master/book-tutorial")}
+          onClick={() => router.push("/artikel")}
         >
           Kembali
         </button>
       </div>
       <br />
 
-      {/* Content */}
-      <form
-        onSubmit={formik.handleSubmit}
-        className="m-2 flex flex-col-reverse lg:flex-row gap-4"
-      >
+      {/* Form Content */}
+      <form onSubmit={formik.handleSubmit} className="m-2 flex flex-col-reverse lg:flex-row gap-4">
         <div className="bg-white p-3 rounded-lg space-y-4 w-full lg:w-[70%]">
-          {/* Title  */}
+          {/* Title */}
           <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Title
-            </label>
+            <label className="block text-sm font-medium text-gray-700 mb-1">Title</label>
             <input
               type="text"
               name="title"
@@ -165,41 +143,33 @@ const FormArtikel: React.FC = () => {
               className="w-full p-1.5 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Masukkan Title"
             />
-            {formik.touched.title && formik.errors.title ? (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.title}
-              </div>
-            ) : null}
+            {formik.touched.title && formik.errors.title && (
+              <div className="text-red-500 text-sm mt-1">{formik.errors.title}</div>
+            )}
           </div>
 
-          {/* Content  */}
+          {/* Content */}
           <div>
             {isLoadingCkeditor && (
               <div className="bg-black bg-opacity-10 flex justify-center items-center ">
-                <TbLoader
-                  className="text-fuchsia-600 animate-spin"
-                  size={100}
-                />
+                <TbLoader className="text-fuchsia-600 animate-spin" size={100} />
               </div>
             )}
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Content
-            </label>
+            <label className="block text-sm font-medium text-gray-700 mb-1">Content</label>
             <CKEditor
               scriptUrl="https://cdn.ckeditor.com/4.20.0/full/ckeditor.js"
               key={refreshCkeditor}
-              config={toolbarConfig}
+              config={{
+                toolbarConfig,
+                versionCheck: false
+              }}
               type="classic"
               initData={formik.values.content}
               onInstanceReady={onEditorReady}
-              onChange={(e: any) => {
-                formik.setFieldValue("content", e.editor.getData());
-              }}
+              onChange={(e: any) => formik.setFieldValue("content", e.editor.getData())}
             />
             {formik.touched.content && formik.errors.content && (
-              <div className="text-red-500 dark:text-red-300 text-xs">
-                {formik.errors.content}
-              </div>
+              <div className="text-red-500 dark:text-red-300 text-xs">{formik.errors.content}</div>
             )}
           </div>
 
@@ -211,12 +181,11 @@ const FormArtikel: React.FC = () => {
             Submit
           </button>
         </div>
+
+        {/* Status Section */}
         <div className="bg-white p-3 rounded-lg space-y-4 w-full lg:w-[30%] h-fit">
-          {/* Status */}
           <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              Status
-            </label>
+            <label className="block text-sm font-medium text-gray-700 mb-1">Status</label>
             <select
               name="status"
               value={formik.values.status}
@@ -227,11 +196,9 @@ const FormArtikel: React.FC = () => {
               <option value="publish">Publish</option>
               <option value="draft">Draft</option>
             </select>
-            {formik.touched.status && formik.errors.status ? (
-              <div className="text-red-500 text-sm mt-1">
-                {formik.errors.status}
-              </div>
-            ) : null}
+            {formik.touched.status && formik.errors.status && (
+              <div className="text-red-500 text-sm mt-1">{formik.errors.status}</div>
+            )}
           </div>
         </div>
       </form>
